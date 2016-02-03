2e994ef0ffef7e3fb256c32f8eb3d686c9ab7714
simchanu29 <simon.chanu@laposte.net>
2016-02-03 22:47:13 +0100

    Objets Finis + Debug Collision J'ai implémenté les objet en y ajoutant les déplacements et la collision. Au passage j'ai réglé un bug qui faisait que seulement le premier objet était repéré.
    J'ai aussi modifié les fonction de déplacement afin de les rendre utilisable pour tout objet, pas seulement le robot.

--- Simulateur/Objects.py
+++ Simulateur/Objects.py
@@ -2,42 +2,53 @@ from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget
 from PyQt5.QtGui import QBrush, QColor, QPainter, QPolygonF
 from PyQt5.QtCore import Qt, QPointF
 
-class SimuObject:
 
-    def __init__(self,movable):
+class GhostShape():
+
+    def __init__(self, nbp, list_of_points):
+        self.shape = QPolygonF(nbp)
+        for i in range(nbp):
+            pt = list_of_points[i]
+            self.shape.replace(i, QPointF(pt[0], pt[1]))
+        self.nbp = nbp
+
+class SimuObject():
+
+    def __init__(self, movable, x, y, heading, nbp, list_of_points):
+
+        #Propriétés de l'objet
         self.movable = movable
-        #TODO
-
-
-class Shape():
-	def __init__(self, nbp, list_of_points):
-		self.shape = QPolygonF(nbp)
-		for i in range(nbp):
-			pt = list_of_points[i]
-			self.shape.replace(i, QPointF(pt[0], pt[1]))
-		self.nbp = nbp
-
-class Cube(SimuObject):
-
-    def __init__(self, size, x, y): #Pour l'instant x et y sont inutiles
-        super().__init__(True)
-        self.size = size
         self.x = x
         self.y = y
-        self.shape = Shape(3, [(0,0), (20,20), (0,20)])
+        self.heading = heading
 
-        #TODO
+        #Propriétés de la représentation graphique
+        self.shape = QPolygonF(nbp)
+        for i in range(nbp):
+            pt = list_of_points[i]
+            self.shape.replace(i, QPointF(pt[0], pt[1]))
+        self.nbp = nbp #nombre de points dans l'objet
+
+        # TODO
+
+class Cube(SimuObject):
+    def __init__(self, size, x, y, heading):  # Pour l'instant x et y sont inutiles
+        self.size = 10
+        super().__init__(True, x, y, heading,  4, [(x - self.size/2, y - self.size/2),
+                                                   (x - self.size/2, y + self.size/2),
+                                                   (x + self.size/2, y + self.size/2),
+                                                   (x + self.size/2, y - self.size/2)])
 
 class Robot(SimuObject):
+    def __init__(self, x, y, heading):  # Pour l'instant x et y sont inutiles
 
-    def __init__(self, x, y): #Pour l'instant x et y sont inutiles
-        super().__init__(True)
+        #Override de x, y , heading pour un test
+        x = 255
+        y = 255
+        heading = 0
+
         self.size = 30
-        self.x = 255
-        self.y = 255
-        self.shape = Shape(4, [(self.x - self.size/2, self.x - self.size/2),
-                               (self.x + self.size/2, self.x - self.size/2),
-                               (self.x + self.size/2, self.x + self.size/2),
-                               (self.x - self.size/2, self.x + self.size/2)])
-
-    pass
+        super().__init__(True, x, y, heading, 4, [(x - self.size / 2, y - self.size / 2),
+                                                  (x + self.size / 2, y - self.size / 2),
+                                                  (x + self.size / 2, y + self.size / 2),
+                                                  (x - self.size / 2, y + self.size / 2)])
--- Simulateur/simu.py
+++ Simulateur/simu.py
@@ -3,7 +3,7 @@
 """
 Pour le moment, le simu se resume a un petit carre qui avance, recule, tourne quand on appuie sur les fleches du clavier
 """
-#TODO : capteurs, objets
+# TODO : capteurs, objets
 
 
 import sys
@@ -15,123 +15,155 @@ from PyQt5.QtCore import Qt, QPointF
 import collisions
 import Objects
 
+
 class Window(QWidget):
-	def __init__(self):
-		super().__init__()
+    def __init__(self):
+        super().__init__()
 
-		self.initGui()
+        self.initGui()
 
-	def initGui(self):
-		self.robot = Objects.Robot(255,255)
-		self.pas = 3#en pixels
+    def initGui(self):
 
-		'''
+        self.pas = 3  # en pixels
+
+        self.allObjects = []
+
+        self.robot = Objects.Robot(255, 255, 0)
+        self.allObjects.append(self.robot)
+
+        '''
 		#les autres figures (utiles pour les collisions)
 		self.ofig = []
 		self.ofig.append(Shape(3, [(35+30,40+30), (45+30,60+30), (15+30, 60+30)]))
 		'''
 
-		#ajout cube
-		self.cubes = []
-		self.cubes.append(Objects.Cube(10,0,0))
+        # ajout cube
+        self.cubes = []
+        self.cubes.append(Objects.Cube(20, 290, 240, 0))
+        self.cubes.append(Objects.Cube(20, 40, 40, 0))
+        self.cubes.append(Objects.Cube(20, 40, 80, 0))
 
-		self.resize(500,500)
-		self.center()
-		self.setWindowTitle('Simulateur')
-		self.show()
 
-	def center(self):
-		screen = QDesktopWidget().screenGeometry()
-		size =	self.geometry()
-		self.move((screen.width()-size.width())/2, (screen.height()-size.height())/2)
+        for cube in self.cubes:
+            self.allObjects.append(cube)
 
-	def paintEvent(self, event):
-		qp = QPainter()
-		qp.begin(self)
-		self.drawPol(qp)
-		qp.end()
+        self.resize(500, 500)
+        self.center()
+        self.setWindowTitle('Simulateur')
+        self.show()
 
-	def tryTrans(self, p0, p2, sens):
-		nbp = self.fig.nbp
-		virtual_fig = Shape(nbp, [(0,0) for i in range(nbp)])
+    def center(self):
+        screen = QDesktopWidget().screenGeometry()
+        size = self.geometry()
+        self.move((screen.width() - size.width()) / 2, (screen.height() - size.height()) / 2)
 
-		p1 = self.fig.shape.at(1)
+    def paintEvent(self, event): #Override de la méthode QWidget
+        qp = QPainter()
+        qp.begin(self)
+        self.drawPol(qp)
+        qp.end()
 
-		#on determine le vecteur de translation a partir des coord du centre et du milieu du trait avant du carre
-		x0 = (abs(p0.x() - p2.x())/2 + min((p0.x(), p2.x())))
-		y0 = (abs(p0.y() - p2.y())/2 + min((p0.y(), p2.y())))
+    def tryTrans(self, movingObject, sens):
 
-		x1 = abs(p0.x() - p1.x())/2 + min((p0.x(), p1.x()))
-		y1 = (abs(p0.y() - p1.y())/2 + min((p0.y(), p1.y())))
+        nbp = movingObject.nbp  # nombre de points
+        virtual_fig = Objects.GhostShape(nbp, [(0, 0) for i in range(nbp)])
 
-		norme = np.sqrt((x1-x0)**2 + (y1-y0)**2)
-		v_t = (sens*(x1-x0)*self.pas/norme, sens*(y1-y0)*self.pas/norme)
-		Trans = np.array([[1,0,v_t[0]],[0,1,v_t[1]]])#matrice de translation
+        p0 = movingObject.shape.at(0)
+        p1 = movingObject.shape.at(1)
+        p2 = movingObject.shape.at(2)
 
-		for i in range(nbp):
-			point = self.fig.shape.at(i)
-			pt = np.array([[point.x()],[point.y()],[1]])
-			new_point = np.dot(Trans, pt)
+        #Détermination de la liste des autres objets
 
-			virtual_fig.shape.replace(i, QPointF(new_point[0], new_point[1]))
+        otherObjects = self.allObjects.copy()
+        otherObjects.remove(movingObject)
 
-		if collisions.check_collisions(virtual_fig, self.ofig, self.geometry()):
-			virtual_fig.shape.swap(self.fig.shape)
+        '''
+        Test pour DEBUG
 
-	def tryRotate(self, p0, p2, theta): # p0, p2, theta
-		nbp = self.fig.nbp
-		virtual_fig = Shape(nbp, [(0,0) for i in range(nbp)])
+        for obj in otherObjects:
+            print('x =',obj.x,'y =',obj.y)
+        '''
 
-		#recuperer les coord du centre de rotation à partir du milieu de la diagonale plus la position du carré
-		x0 = (abs(p0.x() - p2.x())/2 + min((p0.x(), p2.x())))
-		y0 = (abs(p0.y() - p2.y())/2 + min((p0.y(), p2.y())))
+        # on determine le vecteur de translation a partir des coord du centre et du milieu du trait avant du carre
+        x0 = (abs(p0.x() - p2.x()) / 2 + min((p0.x(), p2.x())))
+        y0 = (abs(p0.y() - p2.y()) / 2 + min((p0.y(), p2.y())))
 
-		#definir la matrice de rotation
-		Rot = np.array([[np.cos(theta),-np.sin(theta)], [np.sin(theta), np.cos(theta)]])
+        x1 = abs(p0.x() - p1.x()) / 2 + min((p0.x(), p1.x()))
+        y1 = (abs(p0.y() - p1.y()) / 2 + min((p0.y(), p1.y())))
 
-		#appliquer la rotation a l'ensemble des points
-		for i in range(nbp):
-			point = self.fig.shape.at(i)
-			pt = np.array([[point.x()-x0],[point.y()-y0]])
-			new_point = np.dot(Rot, pt)
+        norme = np.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
+        v_t = (sens * (x1 - x0) * self.pas / norme, sens * (y1 - y0) * self.pas / norme)
+        Trans = np.array([[1, 0, v_t[0]], [0, 1, v_t[1]]])  # matrice de translation
 
-			#mettre a jour les points
-			virtual_fig.shape.replace(i, QPointF(new_point[0]+x0, new_point[1]+y0))
+        #Dessin de la nouvelle figure et suppression de l'autre
+        for i in range(nbp):
+            point = movingObject.shape.at(i)
+            pt = np.array([[point.x()], [point.y()], [1]])
+            new_point = np.dot(Trans, pt)
 
-		if collisions.check_collisions(virtual_fig, self.ofig, self.geometry()):
-			virtual_fig.shape.swap(self.fig.shape)
+            virtual_fig.shape.replace(i, QPointF(new_point[0], new_point[1]))
 
-	def keyPressEvent(self, e):
-		p0 = self.fig.shape.at(0)
-		p2 = self.fig.shape.at(2)
+        #Ckeck de la collision
+        if collisions.check_collisions(virtual_fig, otherObjects, self.geometry()):
+            virtual_fig.shape.swap(movingObject.shape)
 
-		if e.key() == Qt.Key_Up:
-			self.tryTrans( p0,p2, 1)
-		elif e.key() == Qt.Key_Down:
-			self.tryTrans( p0,p2, -1)
-		elif e.key() == Qt.Key_Left:
-			self.tryRotate( p0, p2, -np.pi/8)
-		elif e.key() == Qt.Key_Right:
-			self.tryRotate(p0, p2, np.pi/8)
+    def tryRotate(self, movingObject, theta):  # p0, p2, theta
+        nbp = movingObject.nbp
+        virtual_fig = Objects.GhostShape(nbp, [(0, 0) for i in range(nbp)])
 
-		#mettre a jour la scene
-		self.update()
+        p0 = movingObject.shape.at(0)
+        p2 = movingObject.shape.at(2)
 
-	def drawPol(self, qp):
-		qp.setPen(QColor(0,0,255))
-		qp.setBrush(QColor(255,0,0))
+        #Détermination de la liste des autres objets
+        otherObjects = self.allObjects.copy()
+        otherObjects.remove(movingObject)
 
-		qp.drawConvexPolygon(self.fig.shape)
+        # recuperer les coord du centre de rotation à partir du milieu de la diagonale plus la position du carré
+        x0 = (abs(p0.x() - p2.x()) / 2 + min((p0.x(), p2.x())))
+        y0 = (abs(p0.y() - p2.y()) / 2 + min((p0.y(), p2.y())))
 
-		for shape in self.ofig:
-			qp.drawConvexPolygon(shape.shape)
+        # definir la matrice de rotation
+        Rot = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
 
-		for cube in self.cubes:
-			qp.drawConvexPolygon(cube.shape.shape)
+        # appliquer la rotation a l'ensemble des points
+        for i in range(nbp):
+            point = movingObject.shape.at(i)
+            pt = np.array([[point.x() - x0], [point.y() - y0]])
+            new_point = np.dot(Rot, pt)
+
+            # mettre a jour les points
+            virtual_fig.shape.replace(i, QPointF(new_point[0] + x0, new_point[1] + y0))
+
+        if collisions.check_collisions(virtual_fig, otherObjects, self.geometry()):
+            virtual_fig.shape.swap(movingObject.shape)
+
+    def keyPressEvent(self, e): #Override de la méthode QWidget
+        p0 = self.robot.shape.at(0)
+        p2 = self.robot.shape.at(2)
+
+        if e.key() == Qt.Key_Up:
+            self.tryTrans( self.robot, 1)
+        elif e.key() == Qt.Key_Down:
+            self.tryTrans( self.robot, -1)
+        elif e.key() == Qt.Key_Left:
+            self.tryRotate( self.robot, -np.pi / 16)
+        elif e.key() == Qt.Key_Right:
+            self.tryRotate( self.robot, np.pi / 16)
+
+        # mettre a jour la scene
+        self.update()
+
+    def drawPol(self, qp):
+        qp.setPen(QColor(0, 0, 255))
+        qp.setBrush(QColor(255, 0, 0))
+
+        qp.drawConvexPolygon(self.robot.shape)
+
+        for cube in self.cubes:
+            qp.drawConvexPolygon(cube.shape)
 
 
 if __name__ == '__main__':
-	app = QApplication(sys.argv)
-	w = Window()
-
-	sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    w = Window()
+    sys.exit(app.exec_())
