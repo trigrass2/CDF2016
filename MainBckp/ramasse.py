542bbc8ace93a16c7a7924de06e54b5ea943611a
joris <joris.tillet@ensta-bretagne.org>
2016-03-09 19:17:42 +0100

    Added files via upload

--- MainBckp/ramasse.py
+++ MainBckp/ramasse.py
@@ -6,7 +6,31 @@ Created on Wed Feb 24 18:22:06 2016
 """
 
 import numpy as np
-from math import round, sqrt
+from math import round, sqrt,cos,sin
+
+def limite(coord):
+    if coord[0]-self.r<=0: # si on est le long de la paroi gauche
+            if coord[1]-self.r<=0: # si on est le long de la paroi basse
+                return (self.r,self.r)
+            elif coord[1]+self.r>=3000: # si on est le long de la paroi haute
+                return (self.r, 3000-self.r)
+            else:
+                return  (self.r, coord[1])
+        elif coord[0]+self.r>=2000: # si on est le long de la paroi droite
+            if coord[1]-self.r<=0: # si on est le long de la paroi basse
+                return  (2000-self.r,self.r)
+            elif coord[1]+self.r>=3000: # si on est le long de la paroi haute
+                return  (2000-self.r, 3000-self.r)
+            else:
+                return (2000-self.r, coord[1])
+        else:
+            if coord[1]-self.r<=0: # si on est le long de la paroi basse
+                self.__objectif=(coord[0],self.r)
+            elif coord[1]+self.r>=3000: # si on est le long de la paroi haute
+                self.__objectif=(coord[0], 3000-self.r)
+            else:
+                return (coord[0], coord[1])
+
 
 class Ramasse():
     def __init__(self):
@@ -22,27 +46,9 @@ class Ramasse():
 
     @objectif.setter
     def objectif(self,coord):
-        if coord[0]-self.r<=0: # si on est le long de la paroi gauche
-            if coord[1]-self.r<=0: # si on est le long de la paroi basse
-                self.__objectif=(self.r,self.r)
-            elif coord[1]+self.r>=3000: # si on est le long de la paroi haute
-                self.__objectif=(self.r, 3000-self.r)
-            else:
-                self.__objectif = (self.r, coord[1])
-        elif coord[0]+self.r>=2000: # si on est le long de la paroi droite
-            if coord[1]-self.r<=0: # si on est le long de la paroi basse
-                self.__objectif = (2000-self.r,self.r)
-            elif coord[1]+self.r>=3000: # si on est le long de la paroi haute
-                self.__objectif = (2000-self.r, 3000-self.r)
-            else:
-                self.__objectif = (2000-self.r, coord[1])
-        else:
-            if coord[1]-self.r<=0: # si on est le long de la paroi basse
-                self.__objectif=(coord[0],self.r)
-            elif coord[1]+self.r>=3000: # si on est le long de la paroi haute
-                self.__objectif=(coord[0], 3000-self.r)
-            else:
-                self.__objectif = (coord[0], coord[1])
+        self.__objectif = limite(coord)
+
+
 
 
 
@@ -51,8 +57,16 @@ class Ramasse():
         self.obst = capteurs.distances, mapping.obstacles #TODO
 
     def strategie(self):
-        #raph au boulot
-        pass
+        score_max=0
+        R=100
+        for alpha in range(360):
+            coord=R*cos(alpha),R*sin(alpha)
+            point=limite(coord)
+            score=0
+            for obst in self.obst:
+                d=sqrt
+
+
 
 
     def stop_tout(self):
